20fd458a45dcf805517b8c8d8da3a2765a6d4b24
Yuval Atia <uval4u21@gmail.com>
2017-08-27 11:36:01 +0300

    bla

--- app.py
+++ app.py
@@ -3,4 +3,4 @@ from backend import peachme
 app = peachme.app
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=8002, debug=True, use_reloader=True)
+    app.run(host="localhost", port=8002, debug=True, use_reloader=True)
